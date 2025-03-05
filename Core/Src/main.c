7dcf086dc51e5a2410ccfcd64e8bd88cfe5c8971
AaduMaadu <aadeshsenthil@gmail.com>
2025-03-04 20:09:13 -0600

    Added USB Folder

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
