ebe23254456e3e6a7c095e22d9785fceba69c79b
Brendan Dunlap <bddunla0@gmail.com>
2025-03-07 23:24:58 -0600

    Outlined a library for tracking the rocket's stage of flight.

---
+++ Core/Libraries/RocketFSM/Src/FlightStateAdapter.c
@@ -0,0 +1,6 @@
+/*
+ * General flight logic that would apply to every flight computer firmware belongs
+ * in the FlightState.c file.
+ * This file is for hardware and application dependent code, such as redefining
+ * the weak transition functions so they interface with sensor data streams.
+ */
